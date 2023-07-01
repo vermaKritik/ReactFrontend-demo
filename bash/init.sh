a23a98018c1092186cefec17fee630e3816a8529
kritik <kritikverma2002@gmail.com>
2023-07-01 11:39:56 +0530

    .

---
+++ bash/init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Install Node.js and npm
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# Update npm to the latest version
+sudo npm install -g npm
+
+# Change to the /app directory
+cd /app
+
+# Install dependencies using npm
+npm install
+
+# Build the project
+npm run build
